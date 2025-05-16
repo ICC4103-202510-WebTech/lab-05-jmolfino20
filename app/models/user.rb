7e1d1193f80abab3032fbc5d9d76b8c149d5e0c4
jmolfino20 <jamolfino@miuandes.cl>
2025-05-16 03:36:55 -0400

    lab 8 additions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
 
-    has_many :messages, foreign_key: "sender_id"
+    has_many :messages, foreign_key: "user_id"
     has_many :sent_chats, class_name: "Chat", foreign_key: "sender_id", dependent: :destroy
     has_many :received_chats, class_name: "Chat", foreign_key: "receiver_id", dependent: :destroy 
     has_many :received_messages, through: :received_chats, source: :messages
-    
+
+
 end
 
